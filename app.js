c63007e97ebe3e3fde68fd47dc5694975e5a5da2
Remco Weekers <remco.weekers@gmail.com>
2015-09-17 23:25:24 +0200

    Add item rest service.

--- app.js
+++ app.js
@@ -112,6 +112,29 @@ app.get('/api/n/songs/:id', function(request, response) {
 	});
 });
 
+app.get('/api/n/items/', function(request, response) {
+
+  //this starts initializes a connection pool
+  //it will keep idle connections open for a (configurable) 30 seconds
+  //and set a limit of 20 (also configurable)
+  pg.connect(connString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    // client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    client.query('SELECT id, title, story, type, status, date FROM item', function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+      return response.json({items: result.rows});
+
+    });
+  });
+});
+
 app.listen(3000, function() {
 	console.log('Listening on port 3000');
 });
