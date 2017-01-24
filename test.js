4738a2da7e1d7303c4abbb52a47065b558571185
Remco Weekers <Remco.Weekers@gmail.com>
2017-01-24 14:36:00 +0100

    Update test.js

--- test.js
+++ test.js
@@ -14,12 +14,14 @@ var client = new pg.Client({
       host: '127.0.0.1',
       port: 5432
     });
+client.connect();
 
+/*
 beforeEach(function() {
   console.log("DB connString: " + connString + " on " + process.env.ENV);
   console.log("Node env " + process.env.NODE_ENV);
       client.connect();
-      /*
+      
   pg.connect(connString, function(err, client, done) {
     if(err) {
       return console.error('error fetching client from pool', err);
@@ -29,7 +31,7 @@ beforeEach(function() {
     client.query("DELETE FROM SONG");
     client.query("INSERT INTO SONG(artist, title, firstname, name_index, name_length, date_inserted, user_inserted) " + 
       "VALUES('The Police', 'Roxanne', 'Roxanne', 0, 7, now(), 'Tester')");
-  });*/
+  });
 });
 
 describe('Request the list of songs', function() {
@@ -47,3 +49,4 @@ describe('Request a limited and filtered list of songs', function() {
       .expect(200, done);
   });
 })
+*/
