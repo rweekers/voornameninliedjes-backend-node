534f27aa949cf3dda43120e0ccf1da2461ae6ed0
Remco Weekers <Remco.Weekers@gmail.com>
2017-01-24 22:42:40 +0100

    Update test.js

--- test.js
+++ test.js
@@ -5,23 +5,53 @@ var fs = require('fs');
 var nconf = require('nconf'); 
 
 var pg = require('pg');
-// var connString = 'postgres://' + nconf.get('database:username') + ':' + nconf.get('database:password') + '@' + nconf.get('database:host') + '/' + nconf.get('database:testname');
-var connString = 'postgres://postgres@localhost:5432/travis_ci_test';
-
-var client = new pg.Client({
-      user: 'postgres',
-      database: 'travis_ci_test',
-      hostname: 'localhost',
-      port: 5432
-    });
+var config = {
+  user: 'postgres',
+  database: 'travis_ci_test',
+  password: '',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
+};
+ 
+var pool = new pg.Pool(config);
+ 
+// to run a query we can acquire a client from the pool, 
+// run a query on the client, and then return the client to the pool 
+pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool 
+    done();
+ 
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1 
+  });
+});
+ 
+pool.on('error', function (err, client) {
+  // if an error is encountered by a client while it sits idle in the pool 
+  // the pool itself will emit an error event with both the error and 
+  // the client which emitted the original error 
+  // this is a rare occurrence but can happen if there is a network partition 
+  // between your application and the database, the database restarts, etc. 
+  // and so you might want to handle it and at least log it out 
+  console.error('idle client error', err.message, err.stack)
+})
 
 beforeEach(function() {
-      console.log('beforeEach...');
+      console.log('beforeEach...');/*
       client.connect();
       console.log('connected to database');
       client.query("DROP TABLE IF EXISTS SONG;");
       client.query("CREATE TABLE SONG(artist char(40) NOT NULL, title varchar(40) NOT NULL, firstname varchar(40), name_index integer, name_length integer, date_inserted date, user_inserted varchar(40));");
-      console.log('created table');
+      console.log('created table');*/
 });
 
 /*
