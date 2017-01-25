7cc7b45b6ba52beb58f56b1aba190452b98b1d0e
Remco Weekers <Remco.Weekers@gmail.com>
2017-01-25 21:11:31 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 var request = require('supertest');
 var app = require('./app');
 
+beforeEach(function(done) {
+    // Setup
+    console.log('test before function');
+    console.log('NODE_ENV: ' + nconf.get('NODE_ENV'));
+    console.log('NODE_ENV2: ' + process.env.NODE_ENV);
+});
+
 describe('Request the list of songs', function() {
   it('Returns a 200 status code', function(done) {
     request(app)
