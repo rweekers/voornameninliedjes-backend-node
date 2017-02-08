1350eca3508697f2f1f7b972604167d4c6a93c82
Remco Weekers <Remco.Weekers@gmail.com>
2017-02-08 13:09:08 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var request = require('supertest');
-var app = require('./app');
+var app = require('../app/app');
 
 describe('Request the list of songs', function() {
   it('Returns a 200 status code', function(done) {
